678fe2656d3dfa29054daa067287a5682f9a5d8c
Seth Rubenstein <srubenstein@pewresearch.org>
2022-09-23 11:04:18 -0400

    Defaulting to WP core gap

--- build/columns/index.asset.php
+++ build/columns/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '773b4c4823685620978d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '68291c3c550c369ca399');
