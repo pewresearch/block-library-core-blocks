724e620a9416f5b4806ad0429c804621d50cb97d
Seth Rubenstein <srubenstein@pewresearch.org>
2022-10-10 09:36:14 -0400

    Starting to fix grid controls

--- src/column/grid-controls.jsx
+++ src/column/grid-controls.jsx
@@ -64,168 +64,182 @@ const MARKS = [
 ];
 
 export default function GridControls({ attributes, setAttributes, clientId }) {
-	const { index } = useSelect( select => {
-		const { getBlockIndex, getBlockAttributes, getBlockRootClientId } = select( 'core/block-editor' );
+	const { index } = useSelect((select) => {
+		const { getBlockIndex, getBlockAttributes, getBlockRootClientId } =
+			select('core/block-editor');
 		return {
-			index: getBlockIndex( clientId ) + 1,
+			index: getBlockIndex(clientId) + 1,
 		};
 	});
 
-	const { gridStart, gridSpan, tabletGridSpan, mobileGridSpan, tabletGridStart, mobileGridStart, className } = attributes;
+	const {
+		gridStart,
+		gridSpan,
+		tabletGridSpan,
+		mobileGridSpan,
+		tabletGridStart,
+		mobileGridStart,
+		className,
+	} = attributes;
 
 	const classes = className ? className.split(' ') : [];
 
-	const calculateRatio = (start, span) => {
-		return (start + span - 1) / 12;
-	}
+	const calculateRatio = (start, span) => (start + span - 1) / 12;
 
 	const handleGridSpanChange = (newGridSpan, device = 'desktop') => {
 		classes.forEach((item, index) => {
-			if (item.includes('desktop' !== device ? `${device}-grid__span-` : 'grid__span-')) {
+			if (
+				item.includes(
+					'desktop' !== device ? `${device}-grid__span-` : 'grid__span-',
+				)
+			) {
 				classes.splice(index, 1);
 			}
 		});
 
-		const newGridSpanClassName = 'desktop' !== device ? `column${index}-${device}-grid__span-${newGridSpan}` : `column${index}-grid__span-${newGridSpan}`;
-		if ( ! classes.includes( newGridSpanClassName ) ) {
+		const newGridSpanClassName =
+			'desktop' !== device
+				? `column${index}-${device}-grid__span-${newGridSpan}`
+				: `column${index}-grid__span-${newGridSpan}`;
+		if (!classes.includes(newGridSpanClassName)) {
 			classes.push(newGridSpanClassName);
 		}
 
 		const obj = {
 			className: classes.join(' '),
-		}
+		};
 
-		if ( 'desktop' === device ) {
+		if ('desktop' === device) {
 			obj.gridSpan = newGridSpan;
-		} else if ( 'tablet' === device ) {
+		} else if ('tablet' === device) {
 			obj.tabletGridSpan = newGridSpan;
-		} else if ( 'mobile' === device ) {
+		} else if ('mobile' === device) {
 			obj.mobileGridSpan = newGridSpan;
 		}
 
 		setAttributes(obj);
-	}
+	};
 
 	const handleGridStartChange = (newGridStart, device = 'desktop') => {
 		classes.forEach((item, index) => {
-			if (item.includes('desktop' !== device ? `${device}-grid__start-` : 'grid__start-')) {
+			if (
+				item.includes(
+					'desktop' !== device ? `${device}-grid__start-` : 'grid__start-',
+				)
+			) {
 				classes.splice(index, 1);
 			}
 		});
 
-		const newGridStartClassName = 'desktop' !== device ? `column${index}-${device}-grid__start-${newGridStart}` : `column${index}-grid__start-${newGridStart}`;
-		if ( ! classes.includes( newGridStartClassName ) ) {
+		const newGridStartClassName =
+			'desktop' !== device
+				? `column${index}-${device}-grid__start-${newGridStart}`
+				: `column${index}-grid__start-${newGridStart}`;
+		if (!classes.includes(newGridStartClassName)) {
 			classes.push(newGridStartClassName);
 		}
 
 		const obj = {
 			className: classes.join(' '),
-		}
+		};
 
-		if ( 'desktop' === device ) {
+		if ('desktop' === device) {
 			obj.gridStart = newGridStart;
-		} else if ( 'tablet' === device ) {
+		} else if ('tablet' === device) {
 			obj.tabletGridStart = newGridStart;
-		} else if ( 'mobile' === device ) {
+		} else if ('mobile' === device) {
 			obj.mobileGridStart = newGridStart;
 		}
 
 		setAttributes(obj);
-	}
+	};
 
 	return (
-		<Fragment>
-			<InspectorControls>
-				<PanelBody title="Grid Span">
-					<div className="css-grid-column-controls">
-						<RangeControl
-							label="Desktop Span"
-							value={ gridSpan }
-							onChange={ ( newSpan ) => {
-								handleGridSpanChange(newSpan, 'desktop');
-							} }
-							withInputField={false}
-							min={ 1 }
-							max={ 12 }
-							marks={MARKS}
-						/>
-						<CardDivider/>
-						<RangeControl
-							label="Tablet Span"
-							value={ tabletGridSpan }
-							onChange={ ( newSpan ) => {
-								handleGridSpanChange(newSpan, 'tablet');
-							} }
-							withInputField={false}
-							min={ 1 }
-							max={ 8 }
-							marks={MARKS.filter((item) => item.value <= 8)}
-						/>
-						<CardDivider/>
-						<RangeControl
-							label="Mobile Span"
-							value={ mobileGridSpan }
-							onChange={ ( newSpan ) => {
-								handleGridSpanChange(newSpan, 'mobile');
-							} }
-							withInputField={false}
-							min={ 1 }
-							max={ 4 }
-							marks={MARKS.filter((item) => item.value <= 4)}
-						/>
-					</div>
-				</PanelBody>
+		<InspectorControls>
+			<PanelBody title="Grid Span">
+				<div className="css-grid-column-controls">
+					<RangeControl
+						label="Desktop Span"
+						value={gridSpan}
+						onChange={(newSpan) => {
+							handleGridSpanChange(newSpan, 'desktop');
+						}}
+						withInputField={false}
+						min={1}
+						max={12}
+						marks={MARKS}
+					/>
+					<CardDivider />
+					<RangeControl
+						label="Tablet Span"
+						value={tabletGridSpan}
+						onChange={(newSpan) => {
+							handleGridSpanChange(newSpan, 'tablet');
+						}}
+						withInputField={false}
+						min={1}
+						max={8}
+						marks={MARKS.filter((item) => 8 >= item.value)}
+					/>
+					<CardDivider />
+					<RangeControl
+						label="Mobile Span"
+						value={mobileGridSpan}
+						onChange={(newSpan) => {
+							handleGridSpanChange(newSpan, 'mobile');
+						}}
+						withInputField={false}
+						min={1}
+						max={4}
+						marks={MARKS.filter((item) => 4 >= item.value)}
+					/>
+				</div>
+			</PanelBody>
 
-				<PanelBody
-					title="Grid (Experimental)"
-					initialOpen={false}
-				>
-					<div className="css-grid-column-controls">
-						<BaseControl
-							help="These controls are experimental and currently a work-in-progress. Use them at your own risk, no support will be provided."
-						>
-							<RangeControl
-								label="Desktop Start"
-								value={ gridStart }
-								onChange={ ( newStart ) => {
-									handleGridStartChange(newStart, 'desktop');
-								} }
-								withInputField={false}
-								min={ 1 }
-								max={ 12 }
-								marks={MARKS}
-								initialPosition={0}
-							/>
-							<CardDivider/>
-							<RangeControl
-								label="Tablet Start"
-								value={ tabletGridStart }
-								onChange={ ( newStart ) => {
-									handleGridStartChange(newStart, 'tablet');
-								} }
-								withInputField={false}
-								min={ 1 }
-								max={ 8 }
-								marks={MARKS.filter((item) => item.value <= 8)}
-								initialPosition={0}
-							/>
-							<CardDivider/>
-							<RangeControl
-								label="Mobile Start"
-								value={ mobileGridStart }
-								onChange={ ( newStart ) => {
-									handleGridStartChange(newStart, 'mobile');
-								} }
-								withInputField={false}
-								min={ 1 }
-								max={ 4 }
-								marks={MARKS.filter((item) => item.value <= 4)}
-								initialPosition={0}
-							/>
-						</BaseControl>
-					</div>
-				</PanelBody>
-			</InspectorControls>
-		</Fragment>
+			<PanelBody title="Grid (Experimental)" initialOpen={false}>
+				<div className="css-grid-column-controls">
+					<BaseControl help="These controls are experimental and currently a work-in-progress. Use them at your own risk, no support will be provided.">
+						<RangeControl
+							label="Desktop Start"
+							value={gridStart}
+							onChange={(newStart) => {
+								handleGridStartChange(newStart, 'desktop');
+							}}
+							withInputField={false}
+							min={1}
+							max={12}
+							marks={MARKS}
+							initialPosition={0}
+						/>
+						<CardDivider />
+						<RangeControl
+							label="Tablet Start"
+							value={tabletGridStart}
+							onChange={(newStart) => {
+								handleGridStartChange(newStart, 'tablet');
+							}}
+							withInputField={false}
+							min={1}
+							max={8}
+							marks={MARKS.filter((item) => 8 >= item.value)}
+							initialPosition={0}
+						/>
+						<CardDivider />
+						<RangeControl
+							label="Mobile Start"
+							value={mobileGridStart}
+							onChange={(newStart) => {
+								handleGridStartChange(newStart, 'mobile');
+							}}
+							withInputField={false}
+							min={1}
+							max={4}
+							marks={MARKS.filter((item) => 4 >= item.value)}
+							initialPosition={0}
+						/>
+					</BaseControl>
+				</div>
+			</PanelBody>
+		</InspectorControls>
 	);
-};
+}
