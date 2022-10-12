83fa7cb59b95f058700118d318bd180bba2ae52d
Seth Rubenstein <srubenstein@pewresearch.org>
2022-10-12 14:29:28 -0400

    Setting up proper develop branch as we get closer to v1 release

--- src/_WIP/table-of-contents/edit.js
+++
@@ -1,3 +0,0 @@
-// https://github.com/WordPress/gutenberg/blob/trunk/packages/block-library/src/table-of-contents/edit.js
-// Last Updated: 2021-05-18T16:00:00.000Z
-// We need to only get latest headings that have attr isChapter set to true, this is also why it would be great to have a filter here for what is received.
--- src/_WIP/table-of-contents/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// chapters
-// add attribute for "highlight current chapter" which would highlight the current chapter in the table of contents.
